4aa857d16fa3af2c3c724ef3e330b17a8d170adb
vishalsinghpoker <vishal.singh@hashtagpoker.com>
2022-05-26 11:19:54 +0530

    Initial React setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import UserList from './pages/UserList/UserList';
+import UserView from './pages/UserList/UserView';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import React from 'react'; 
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <UserList />
+      <UserView />
     </div>
+    </Router>
   );
 }
 
---
+++ src/pages/UserList/UserList.js
@@ -0,0 +1,12 @@
+import Card from '../../components/Card/Card';
+
+const Home = () => {
+	return (
+		<div>
+			Home
+      		<Card />
+		</div>
+	)
+}
+
+export default Home;
